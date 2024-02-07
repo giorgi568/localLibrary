b29ea4c17094274ef851a5058c8f851eba02f85b
giorgi568 <mateshvili02@mail.ru>
2024-02-08 03:29:58 +0400

    added database model schemas and data

---
+++ models/genre.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const genreSchema = new Schema({
+  name: {
+    type: Schema.Types.String,
+    minLength: 3,
+    maxLength: 100,
+    required: true,
+  },
+});
+
+genreSchema.virtual('url').get(function () {
+  return `catalog/genre/${this._id}`;
+});
+
+module.exports = mongoose.model('genere', genreSchema);
