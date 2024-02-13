12afa11c2c1e4dac6815baafd9529f8e03084e75
giorgi568 <mateshvili02@mail.ru>
2024-02-13 21:51:58 +0400

    added routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.redirect('/catalog');
 });
 
 module.exports = router;
